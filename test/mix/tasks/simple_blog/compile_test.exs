e374db7179c9ccf7219da0153ed19d815df85f62
Vinícius <vba321@hotmail.com>
2023-11-06 12:49:38 -0300

    Move blog_test directory

--- test/mix/tasks/simple_blog/compile_test.exs
+++ test/mix/tasks/simple_blog/compile_test.exs
@@ -3,29 +3,29 @@ defmodule Mix.Tasks.SimpleBlog.CompileTest do
 
   describe "run/1" do
     setup do
-      on_exit(fn -> File.rm_rf("output_test") end)
+      on_exit(fn -> File.rm_rf("test/output") end)
     end
 
     test "create a directory to static blog" do
-      Mix.Tasks.SimpleBlog.Compile.run(["blog_test", "output_test"])
-      assert File.exists?("output_test")
+      Mix.Tasks.SimpleBlog.Compile.run(["test/blog", "test/output"])
+      assert File.exists?("test/output")
     end
 
     test "converts posts to html" do
-      Mix.Tasks.SimpleBlog.Post.run(["My First Blog Post", "blog_test"])
-      Mix.Tasks.SimpleBlog.Compile.run(["blog_test", "output_test"])
+      Mix.Tasks.SimpleBlog.Post.run(["My First Blog Post", "test/blog"])
+      Mix.Tasks.SimpleBlog.Compile.run(["test/blog", "test/output"])
 
       today = Date.utc_today() |> Date.to_string()
-      dir = SimpleBlog.Post.generate_html_dir(%SimpleBlog.Post{date: today}, "output_test/posts")
+      dir = SimpleBlog.Post.generate_html_dir(%SimpleBlog.Post{date: today}, "test/output/posts")
 
       assert File.exists?(dir <> "my-first-blog-post.html")
 
-      on_exit(fn -> File.rm("blog_test/_posts/#{today}-my-first-blog-post.md") end)
+      on_exit(fn -> File.rm("test/blog/_posts/#{today}-my-first-blog-post.md") end)
     end
 
     test "creates css files" do
-      Mix.Tasks.SimpleBlog.Compile.run(["blog_test", "output_test"])
-      css_dir = "output_test/css/"
+      Mix.Tasks.SimpleBlog.Compile.run(["test/blog", "test/output"])
+      css_dir = "test/output/css/"
 
       assert File.exists?(css_dir <> "_solarized-light.css")
       assert File.exists?(css_dir <> "plain.css")
@@ -34,8 +34,8 @@ defmodule Mix.Tasks.SimpleBlog.CompileTest do
     end
 
     test "creates images files" do
-      Mix.Tasks.SimpleBlog.Compile.run(["blog_test", "output_test"])
-      images_dir = "output_test/images/"
+      Mix.Tasks.SimpleBlog.Compile.run(["test/blog", "test/output"])
+      images_dir = "test/output/images/"
 
       assert File.exists?(images_dir <> "avatar.png")
     end
--- test/mix/tasks/simple_blog/post_test.exs
+++ test/mix/tasks/simple_blog/post_test.exs
@@ -19,22 +19,22 @@ defmodule Mix.Tasks.SimpleBlog.PostTest do
     test "show success message for created blog post" do
       message =
         capture_io(fn ->
-          Mix.Tasks.SimpleBlog.Post.run(["My First Blog Post", "blog_test"])
+          Mix.Tasks.SimpleBlog.Post.run(["My First Blog Post", "test/blog"])
         end)
 
       today = Date.utc_today() |> Date.to_string()
 
-      assert message == "Blog post created at blog_test/_posts/#{today}-my-first-blog-post.md\n\n"
-      on_exit(fn -> File.rm("blog_test/_posts/#{today}-my-first-blog-post.md") end)
+      assert message == "Blog post created at test/blog/_posts/#{today}-my-first-blog-post.md\n\n"
+      on_exit(fn -> File.rm("test/blog/_posts/#{today}-my-first-blog-post.md") end)
     end
 
     test "creates a new markdown file for blog post" do
-      Mix.Tasks.SimpleBlog.Post.run(["My First Blog Post", "blog_test"])
+      Mix.Tasks.SimpleBlog.Post.run(["My First Blog Post", "test/blog"])
 
       today = Date.utc_today() |> Date.to_string()
 
-      assert File.exists?("blog_test/_posts/#{today}-my-first-blog-post.md")
-      on_exit(fn -> File.rm("blog_test/_posts/#{today}-my-first-blog-post.md") end)
+      assert File.exists?("test/blog/_posts/#{today}-my-first-blog-post.md")
+      on_exit(fn -> File.rm("test/blog/_posts/#{today}-my-first-blog-post.md") end)
     end
 
     test "show error message when blog does not exist" do
